a4426670342795a96f1437caf5fdcd10f4499141
Damar2121 <damar.vivoy51@gmail.com>
2025-10-15 11:51:13 +0700

    Implement POST /mahasiswa endpoint for data submission

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ let mysql = require ('mysql2');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json());
+
+app.post('/mahasiswa', (req, res) => {
+
+    const data = req.body;
+    console.log('Data diterima:', data);
+
+    res.status(201).json({
+        message: 'Data mahasiswa berhasil diterima!',
+        data: data
+    });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
