4e34a3ad064afd304612c780b852bd1709e4c75b
Damar2121 <damar.vivoy51@gmail.com>
2025-10-15 11:38:24 +0700

    feat: Initial server setup with Express and MySQL connection

---
+++ index.js
@@ -0,0 +1,28 @@
+const express =  require ('express');
+let mysql = require ('mysql2');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    port: '3309',
+    password: '200421D@m2121',
+    database: 'mahasiswa',
+})
+
+db.connect((err) => {
+    if(err){
+        console.log('error connecting to Mysql: ' +err.stack);
+        return;
+    }
+    console.log('Connected t0 Mysql succesfully');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
